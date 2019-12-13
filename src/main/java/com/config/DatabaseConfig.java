e02553ddf3b901f9d45049bf3b61f6e6799f04b5
oleg.maliy14@gmail.com <q2301299266>
2019-12-13 21:56:48 +0200

    -0- 13.12.2019

--- src/main/java/com/config/DatabaseConfig.java
+++ src/main/java/com/config/DatabaseConfig.java
@@ -1,20 +1,32 @@
 package com.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableJpaRepositories("com.repo")
 @EnableTransactionManagement
+@PropertySource("classpath:db.properties")
 @ComponentScan("com")
 public class DatabaseConfig {
 
-    @Autowired
-    private Environment env;
+    private final Environment env;
 
-    //com.mysql.jdbc.Driver
+    @Autowired
+    public DatabaseConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return null;
+    }
 }
